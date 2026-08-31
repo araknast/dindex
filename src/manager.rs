f23e9d83c5463a3daa22bda5d047c81cbecb8160
araknast <araknast@protonmail.com>
2026-08-31 15:04:21 -0500

    more tests for manager

--- src/manager.rs
+++ src/manager.rs
@@ -5,7 +5,7 @@ use sha2::{Digest, Sha256};
 use std::{fmt::Debug, fs, io, path::Path};
 
 mod dindex;
-
+// TODO: object_id->version_id
 pub struct DIndexManager {
     data_root: String,
 }
@@ -70,14 +70,18 @@ mod test {
 
     use crate::manager::{DIndexManager, dindex::DIndexObjectId};
 
-    const FILE1: &str = "lines\nof\nthe\nfile\n";
-    const FILE2: &str = "the\nfile\n";
-    const FILE3: &str = "the\nfile\nlines\nof\n";
-    const FILE4: &str = "some\nnew\nlines\nof\nimportance\nfor\nthe\nfile\nhere\n";
-    const FILE5: &str = "whole\ndifferent\ntext\n";
+    const FILE_NAME: &str = "file.txt";
+
+    const FILE_VERSIONS: [&str; 5] = [
+        "lines\nof\nthe\nfile\n",
+        "the\nfile\n",
+        "the\nfile\nlines\nof\n",
+        "some\nnew\nlines\nof\nimportance\nfor\nthe\nfile\nhere\n",
+        "whole\ndifferent\ntext\n",
+    ];
 
     #[test]
-    fn create_new_dindex() {
+    fn create_one_file() {
         let tmp = assert_fs::TempDir::new().unwrap();
         let dir = tmp.child("indexes");
         let data_root: &str = &dir.path().to_string_lossy();
@@ -85,10 +89,44 @@ mod test {
         let manager = DIndexManager::new(data_root);
         manager.create_data_root().unwrap();
 
-        let parent_id = DIndexObjectId::from_object_data(FILE1);
-        let object_id = manager.insert("file.txt", FILE1, parent_id).unwrap();
+        let parent_id = DIndexObjectId::from_object_data(FILE_VERSIONS[0]);
+        let object_id = manager
+            .insert(FILE_NAME, FILE_VERSIONS[0], parent_id)
+            .unwrap();
 
-        let object = manager.get("file.txt", object_id).unwrap().unwrap();
-        assert_eq!(FILE1, object);
+        let object = manager.get(FILE_NAME, object_id).unwrap().unwrap();
+        assert_eq!(FILE_VERSIONS[0], object);
+    }
+
+    #[test]
+    fn modify_one_file() {
+        let tmp = assert_fs::TempDir::new().unwrap();
+        let dir = tmp.child("indexes");
+        let data_root: &str = &dir.path().to_string_lossy();
+
+        let manager = DIndexManager::new(data_root);
+        manager.create_data_root().unwrap();
+
+        // insert the root version
+        let mut parent_id = DIndexObjectId::from_object_data(FILE_VERSIONS[0]);
+        manager
+            .insert(FILE_NAME, FILE_VERSIONS[0], parent_id)
+            .unwrap();
+
+        let mut version_ids = Vec::with_capacity(FILE_VERSIONS.len());
+
+        // simulate file updates
+        for version in FILE_VERSIONS {
+            let object_id = manager.insert(FILE_NAME, version, parent_id).unwrap();
+            version_ids.push(object_id);
+            let stored_data = manager.get(FILE_NAME, object_id).unwrap().unwrap();
+            assert_eq!(version, stored_data);
+            parent_id = object_id;
+        }
+
+        for i in 0..FILE_VERSIONS.len() {
+            let stored_data = manager.get(FILE_NAME, version_ids[i]).unwrap().unwrap();
+            assert_eq!(stored_data, FILE_VERSIONS[i])
+        }
     }
 }
