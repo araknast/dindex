6d542a39c8e0accd63f3cdc30e0ca8093612f34d
araknast <araknast@protonmail.com>
2026-08-30 23:09:58 -0500

    dindex manager

--- src/manager.rs
+++ src/manager.rs
@@ -1,7 +1,8 @@
 use thiserror::Error;
 
-use crate::manager::dindex::{DIndex, DIndexKey, DeserializationError};
-use std::{fs, io, path::Path};
+use crate::manager::dindex::{DIndex, DIndexObjectId, DeserializationError};
+use sha2::{Digest, Sha256};
+use std::{fmt::Debug, fs, io, path::Path};
 
 mod dindex;
 
@@ -25,13 +26,33 @@ impl DIndexManager {
     }
 
     fn load_dindex(&self, name: &str) -> Result<DIndex, DIndexLoadError> {
-        let path = Path::new(&self.data_root).join(name);
+        let path = Path::new(&self.data_root).join(hex::encode(name));
         DIndex::try_from(fs::read(path)?).map_err(Into::into)
     }
     fn persist_dindex(&self, index: DIndex) -> Result<(), io::Error> {
-        let path = Path::new(&self.data_root).join(index.name());
+        let name_hash: String = hex::encode(Sha256::digest(index.name()));
+        let path = Path::new(&self.data_root).join(name_hash);
         fs::write(path, Vec::<u8>::from(index))
     }
-    fn get(name: &str, key: DIndexKey) {}
-    fn insert(name: &str, data: String) {}
+    fn get(&self, name: &str, key: DIndexObjectId) -> Result<Option<String>, DIndexLoadError> {
+        let index = self.load_dindex(name)?;
+        Ok(index.get_object_data(key))
+    }
+
+    fn insert(
+        &self,
+        name: &str,
+        data: &str,
+        parent: DIndexObjectId,
+    ) -> Result<DIndexObjectId, DIndexLoadError> {
+        let result = self.load_dindex(name);
+
+        let mut index = if let Err(DIndexLoadError::Io(_)) = result {
+            DIndex::new(name)
+        } else {
+            result?
+        };
+
+        Ok(index.insert_object(data, parent))
+    }
 }
