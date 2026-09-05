58b4382aa855f6513be7b0bd4f3d2bca7b1dd9d4
araknast <araknast@protonmail.com>
2026-09-05 02:16:44 -0500

    start work on snap.rs

--- src/bin/snap.rs
+++ src/bin/snap.rs
@@ -4,12 +4,14 @@ use dindex::snap_manager::SnapshotManager;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
+    if args.len() < 3 {
         println!("Not enough arguments.");
         std::process::exit(1)
     }
-    let data_root = &args[1];
+    let target_dir = &args[1];
+    let data_root = &args[2];
 
     let manager: SnapshotManager = SnapshotManager::new(data_root);
-    // manager.new_snap();
+    let _ = manager.init_dirs();
+    let _ = manager.snapshot_from_dir(target_dir);
 }
