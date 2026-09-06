886afa8cadbd2896c1cb28377d7fa4e268a51b5c
araknast <araknast@protonmail.com>
2026-09-06 15:40:54 -0500

    inserting data that is alreay stored should be a noop

--- src/dindex.rs
+++ src/dindex.rs
@@ -288,8 +288,7 @@ impl DIndex {
     pub fn insert_version(&mut self, version_data: &str) -> DIndexVersionId {
         let version_id = DIndexVersionId::from_version_data(version_data);
         let data_key = self.key_from_data(version_data);
-        self.version_map.insert(
-            version_id,
+        self.version_map.entry(version_id).or_insert(
             DIndexVersion {
                 prev: self.head,
                 next: version_id,
