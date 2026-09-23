de6ac78c1c6c212d4134d39c0118872a91cd43ee
araknast <araknast@protonmail.com>
2026-09-22 20:57:09 -0500

    add verify script

---
+++ verify.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+re=$(realpath ./target/release/re)
+original=$(realpath $1)
+data="$(realpath $2)"
+output="$(realpath $3)"
+snaplog="$data/snap.log"
+
+if test -z $snaplog || test -z $output || test -z $original
+then
+	exit
+fi
+
+prevdir="$PWD"
+cd "$original"
+git reset --hard origin/HEAD
+cd "$prevdir"
+while read line
+do
+	snap_id=$(echo $line | cut -f1 -d' ')
+	commit_id=$(echo $line | cut -f2 -d' ')
+	$re "$data" $snap_id "$output"
+	cd "$original"
+	git reset --hard $commit_id
+	cd "$prevdir"
+	diff -r "$output" "$original"
+	rm -r "$output"
+	mkdir -p "$output"
+done < "$snaplog"
